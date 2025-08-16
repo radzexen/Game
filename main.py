3e686762734f7e3e0877aaea5b1af374c83efe32
radzexen <xeniyarad@gmail.com>
2025-08-16 22:58:57 +0200

    base code done

--- main.py
+++ main.py
@@ -2,27 +2,36 @@ import pygame
 import random
 pygame.init()
 
-SCREEN_WIDTH = 1536
-SCREEN_HEIGHT = 1024
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Fly Killer")
-icon = pygame.image.load("img/flykiller.png")
+icon = pygame.image.load("img/flykiller_icon.png")
 pygame.display.set_icon(icon)
 
 background_img = pygame.image.load("img/flykiller_background.png")
+background_img = pygame.transform.scale(background_img, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 target_img = pygame.image.load("img/flykiller_target.png")
-targer_width = 50
-targer_height = 50
-
-target_x = random.randint(0, SCREEN_WIDTH - targer_width)
-target_y = random.randint(0, SCREEN_HEIGHT - targer_height)
-
+target_width = 50
+target_height = 50
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 
 running = True
 while running:
-    pass
+    screen.blit(background_img, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
 
 pygame.quit()
 
